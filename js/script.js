3b8fa7cf28edbb4b17fd3c42dc1a73aac00cdb47
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-11-21 18:37:54 +0000

    Exceeds

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
+const body = document.querySelector('body');
+
 /*----------------------------------
            * `quotes` array
 -----------------------------------*/
@@ -11,40 +13,50 @@ const quoteArray = [
     quote: "The way to get started is to quit talking and begin doing.",
     author: "Walt Disney",
     citation: "speech",
-    year: 1971
+    year: 1971,
+    cathegory: "fiction"
   },
   {
     quote: "It is during our darkest moments that we must focus to see the light.",
     author: "Aristotle",
     citation: "song",
-    year: 650
+    year: 650,
+    cathegory: "thiller"
   },
   {
     quote: "Whoever is happy will make others happy too.",
-    author: "Anne Frank"
+    author: "Anne Frank",
+    cathegory: "action"
   },
   {
     quote: "Life is a succession of lessons which must be lived to be understood.",
     author: "Ralph Waldo Emerson",
     citation: "book",
+    cathegory: "fiction"
   },
   {
     quote: "Never let the fear of striking out keep you from playing the game.",
     author: "Babe Ruth",
     citation: "movie",
-    year: 2008
+    year: 2008,
+    cathegory: "drama"
   }
 ];
-/***
- * `getRandomQuote` function
-***/
+/*--------------------------------------
+     `getRandomQuote` function
+--------------------------------------*/
+
+function randomNumber() {
+  let number = Math.floor(Math.random() * 256) + 1;
+  return number;
+}
 
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quoteArray.length);
   return quoteArray[randomNumber];
 }
 /*---------------------------------
-    Create `printQuote` function
+     Create `printQuote` function
 -----------------------------------*/
 
 function printQuote() {
@@ -58,13 +70,26 @@ function printQuote() {
   if ( zmienna.year ) {
     html += '<span class="year">' + zmienna.year + '</span>';
   }
+  if ( zmienna.cathegory ) {
+    html += '<span class="cathegory">' + zmienna.cathegory + '</span>';
+  }
   html += '</p>';
   document.getElementById('quote-box').innerHTML = html;
+  let n1 = randomNumber();
+  let n2 = randomNumber();
+  let n3 = randomNumber();
+  let string = "rgba(" + n1 + ", " + n2 + ", " + n3 + ", .95)";
+  body.style.backgroundColor = string;
 }
 
 
 /*------------------------------------------
    Event Listener for clicking button
+   OR
+   Display changing every 4 sekonds
 -------------------------------------------*/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+document.getElementById('load-quote').style.display = 'none';
+window.setInterval(printQuote, 3000);
